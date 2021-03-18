fd98cf644af033c31b0e16aa90d17767400b61aa
Dmitry Shovkoplias <dmitryshovk@gmail.com>
2021-03-18 12:06:10 +0200

    final task1

--- script/custom.js
+++ script/custom.js
@@ -3,7 +3,6 @@ window.onload = function () {
 }
 
 
-
 document.addEventListener('touchstart', handleTouchStart, false);        
 document.addEventListener('touchmove', handleTouchMove, false);
 
@@ -24,17 +23,23 @@ function handleTouchStart(evt) {
 function handleTouchMove(evt) {
     if ( ! xDown || ! yDown ) {
         return;
-    }F
+    }
     var xUp = evt.touches[0].clientX;                                    
     var yUp = evt.touches[0].clientY;
     var xDiff = xDown - xUp;
     var yDiff = yDown - yUp;
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
         if ( xDiff > 0 ) {
           plusSlide()
         } else {
           minusSlide()
         }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
     }
     xDown = null;
     yDown = null;                                             
